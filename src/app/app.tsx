d30d5e7a2933eaafe9b9954c8df69570a8b0cc10
joaquinrmi <joaquinruaimi@gmail.com>
2021-11-22 16:04:45 -0300

    Update App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Weather from "../pages/weather";
+
 import "./app.scss";
 
 const App: React.FunctionComponent = () =>
 {
-    return <></>;
+    return <>
+        <Weather />
+    </>;
 };
 
 export default App;
