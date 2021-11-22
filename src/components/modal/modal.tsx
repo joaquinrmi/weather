b78b22e02e64f7b33b9eed091a984cca7d3df500
joaquinrmi <joaquinruaimi@gmail.com>
2021-11-22 16:04:10 -0300

    Add Modal component

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+
+import "./modal.scss";
+
+export interface Props
+{
+    id: string;
+}
+
+export interface ModalElement extends HTMLDivElement
+{
+    isOpen(): boolean;
+    open(): void;
+    close(): void;
+}
+
+const Modal: React.FunctionComponent<Props> = (props) =>
+{
+    useEffect(() =>
+    {
+        const modal = document.getElementById(props.id) as ModalElement;
+
+        modal.isOpen = () =>
+        {
+            return modal.classList.contains("visible") && !modal.classList.contains("closing");
+        };
+
+        modal.open = () =>
+        {
+            modal.classList.add("visible");
+        };
+
+        modal.close = () =>
+        {
+            modal.classList.add("closing");
+            setTimeout(() =>
+            {
+                modal.classList.remove("visible");
+                modal.classList.remove("closing");
+            },
+            250);
+        };
+
+        const checkEscape = (ev: KeyboardEvent) =>
+        {
+            if(ev.key === "Escape")
+            {
+                modal.close();
+            }
+        }
+
+        document.addEventListener("keydown", checkEscape);
+
+        const closeButton = document.getElementById(`close-${props.id}`) as HTMLSpanElement;
+        
+        closeButton.onclick = () =>
+        {
+            modal.close();
+        };
+
+        return () =>
+        {
+            document.removeEventListener("keydown", checkEscape);
+        };
+    },
+    []);
+
+    return <div id={props.id} className="modal">
+        <header>
+            <i id={`close-${props.id}`} className="fi fi-rr-angle-left"></i>
+        </header>
+
+        <section>
+            {props.children}
+        </section>
+    </div>;
+};
+
+export default Modal;
