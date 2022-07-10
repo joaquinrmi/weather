fa38ed24222796f870b9e72b48015686a6ca012d
joaquinrmi <joaquinruaimi@gmail.com>
2022-07-10 12:04:55 -0300

    Add 'onClick()' to Button Props interface

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
 import "./button.scss";
 
@@ -6,11 +6,17 @@ export interface Props
 {
     id: string;
     content: string;
+
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 const Button: React.FunctionComponent<Props> = (props) =>
 {
-    return <button id={props.id} className="button">
+    return <button
+        id={props.id}
+        className="button"
+        onClick={props.onClick}
+    >
         <span className="content">
             {props.content}
         </span>
