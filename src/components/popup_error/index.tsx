72889bd6e59d0f8a7d1fbe57ff09501f08ff59d2
joaquinrmi <joaquinruaimi@gmail.com>
2022-07-10 15:33:25 -0300

    Add PopupError component

---
+++ src/components/popup_error/index.tsx
@@ -0,0 +1,4 @@
+import PopupError from "./popup_error";
+
+export default PopupError;
+export * from "./popup_error";
---
+++ src/components/popup_error/popup_error.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+
+import "./popup_error.scss";
+
+export interface Props
+{
+    id: string;
+    title: React.ReactNode;
+    content: React.ReactNode;
+
+    closeRequest(): void;
+}
+
+const PopupError: React.FunctionComponent<Props> = (props) =>
+{
+    useEffect(() =>
+    {
+        setTimeout(() =>
+        {
+            const element = document.getElementById(props.id) as HTMLDivElement;
+            if(element === null)
+            {
+                return;
+            }
+
+            element.classList.add("end");
+
+            const animation = element.querySelector(".popup-animation") as HTMLDivElement;
+            if(animation === null)
+            {
+                return;
+            }
+
+            animation.classList.add("end");
+        }, 200);
+    });
+
+    return <div id={props.id} className="popup-error">
+        <div className="popup-animation">
+            <section className="popup-body">
+                <header className="popup-header">
+                    {props.title}
+                </header>
+
+                <section className="popup-content">
+                    {props.content}
+                </section>
+
+                <footer className="popup-footer">
+                    <button className="popup-close">
+                        Cerrar
+                    </button>
+                </footer>
+            </section>
+        </div>
+    </div>;
+};
+
+export default PopupError;
